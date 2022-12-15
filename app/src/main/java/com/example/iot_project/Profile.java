d1710a2b8ffefc2cdd23910a3b345a62a53494d8
Dogu <doguziyan@gmail.com>
2022-12-15 17:14:22 +0100

    Permissions (working :)

--- app/src/main/java/com/example/iot_project/Profile.java
+++ app/src/main/java/com/example/iot_project/Profile.java
@@ -1,5 +1,6 @@
 package com.example.iot_project;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class Profile extends Fragment {
 
